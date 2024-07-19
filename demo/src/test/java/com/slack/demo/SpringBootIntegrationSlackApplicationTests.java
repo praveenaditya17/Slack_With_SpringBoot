a8651dfe167aaaf70c8c90653742b644a49bea05
Praveen Aditya <pk145119@gmail.com>
2024-07-19 13:55:35 +0530

    springBoot integration with slack

---
+++ demo/src/test/java/com/slack/demo/SpringBootIntegrationSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slack.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntegrationSlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
